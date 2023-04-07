57248ed54b91084fd409ad64e635b4370dbcddcb
cade-b <cadeballew@gmail.com>
2023-04-06 23:32:01 -0700

    added basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,11 @@ using RecurrenceCoefficients
 using Test
 
 @testset "core tests" begin
-    @test 1 == 1
+    bands = [-3.0 -2.0; 2.0 3.0]
+    (avec,bvec) = get_n_coeffs(bands,50)
+
+    tol = 1e-12
+    @test maximum(abs.(avec))<tol
+    @test abs(bvec[50]-0.5)<tol
+    @test abs(bvec[51]-2.5)<tol
 end
